bff4f3d3224271891cb9d0859aa7b2b51c1e4557
reinapeng <reina830521@gmail.com>
2021-03-18 10:59:20 +0800

    修改表單樣式

---
+++ MyHR_Web/Satus/eCheckSatus.cs
@@ -0,0 +1,15 @@
+﻿using MyHR_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHR_Web.Satus
+{
+    public class eCheckSatus
+    {
+        internal TTravelExpenseApplication iv_etravel = null;
+        
+       
+    }
+}
