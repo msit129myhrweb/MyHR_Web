06bf120fe0a16d15177eae9734ca0a235353fe5d
王惟臻 <jolin1985512@yahoo.com.tw>
2021-03-28 01:24:40 +0800

    0327_RepairList權限

--- MyHR_Web/Controllers/RepairController.cs
+++ MyHR_Web/Controllers/RepairController.cs
@@ -22,97 +22,111 @@ namespace MyHR_Web.Views.Home
         {
             ViewData[CDictionary.CURRENT_LOGINED_USERNAME] = HttpContext.Session.GetString(CDictionary.CURRENT_LOGINED_USERNAME);
             ViewBag.Session_USERID_USERDEPARTMENTID = int.Parse(HttpContext.Session.GetString(CDictionary.CURRENT_LOGINED_USERDEPARTMENTID));
+            string USERDEPARTMENTID = HttpContext.Session.GetString(CDictionary.CURRENT_LOGINED_USERDEPARTMENTID);
             ViewBag.Session_USERIDSERID = int.Parse( HttpContext.Session.GetString(CDictionary.CURRENT_LOGINED_USERID));
             
-            
+            ViewBag.Session_USERJOBTITLE= HttpContext.Session.GetString(CDictionary.CURRENT_LOGINED_USERJOBTITLE);
+            int userid= int.Parse(HttpContext.Session.GetString(CDictionary.CURRENT_LOGINED_USERID));
+           
 
-            IEnumerable<TRepair> table = null;
-
-            if (!string.IsNullOrEmpty(Request.ContentType))
+            IEnumerable< TRepair > table = null;
+            if (ViewBag.Session_USERID_USERDEPARTMENTID == 4 || ViewBag.Session_USERID_USERDEPARTMENTID == 5 && ViewBag.Session_USERJOBTITLE == "經理") 
             {
-                string Srepairnumber = Request.Form["repairnumber"];
-                string SrepairdateStart = Request.Form["repairdateStart"];
-                string SrepairdateEnd = Request.Form["repairdateEnd"];
-                string Srepaircontent = Request.Form["repaircontent"];
-
-                //單號不為空
-                if (!string.IsNullOrEmpty(Srepairnumber))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CRepairNumber == (int.Parse(Srepairnumber))
-                            orderby r.CAppleDate descending
-                            select r;
-                }//開始日期 結束日期 內容不為空
-                else if (!string.IsNullOrEmpty(SrepairdateStart) && !string.IsNullOrEmpty(SrepairdateEnd) && !string.IsNullOrEmpty(Srepaircontent))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CContentofRepair.Contains(Srepaircontent) && r.CAppleDate <= DateTime.Parse(SrepairdateEnd) && r.CAppleDate >= DateTime.Parse(SrepairdateStart)
-                            orderby r.CAppleDate descending
-                            select r;
-                }//開始日期 結束日期不為空
-                else if (!string.IsNullOrEmpty(SrepairdateStart) && !string.IsNullOrEmpty(SrepairdateEnd))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CAppleDate <= DateTime.Parse(SrepairdateEnd) && r.CAppleDate >= DateTime.Parse(SrepairdateStart)
-                            orderby r.CAppleDate descending
-                            select r;
-                }//開始日期  內容不為空
-                else if (!string.IsNullOrEmpty(SrepairdateStart) && !string.IsNullOrEmpty(Srepaircontent))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CAppleDate >= DateTime.Parse(SrepairdateStart) && r.CContentofRepair.Contains(Srepaircontent)
-                            orderby r.CAppleDate descending
-                            select r;
-                }
-                // 結束日期 內容不為空
-                else if ( !string.IsNullOrEmpty(SrepairdateEnd) && !string.IsNullOrEmpty(Srepaircontent))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CAppleDate <= DateTime.Parse(SrepairdateEnd) && r.CContentofRepair.Contains(Srepaircontent)
-                            orderby r.CAppleDate descending
-                            select r;
-                }//開始日期不為空
-                else if (!string.IsNullOrEmpty(SrepairdateStart))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CAppleDate >= DateTime.Parse(SrepairdateStart)
-                            orderby r.CAppleDate descending
-                            select r;
-                }//結束日期不為空
-                else if (!string.IsNullOrEmpty(SrepairdateEnd))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CAppleDate <= DateTime.Parse(SrepairdateEnd)
-                            orderby r.CAppleDate descending
-                            select r;
-                }//內容不為空
-                else
-                if (!string.IsNullOrEmpty(Srepaircontent))
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            where r.CContentofRepair.Contains(Srepaircontent)
-                            orderby r.CAppleDate descending
-                            select r;
-                }
-                else
-                {
-                    table = from r in (new dbMyCompanyContext()).TRepairs
-                            orderby r.CAppleDate descending
-                            select r;
-                }
-            }
+                 table = from r in (new dbMyCompanyContext()).TRepairs
+                         where (r.CRepairCategory.Contains("資訊") && int.Parse(USERDEPARTMENTID) == 4) || (r.CRepairCategory.Contains("總務") && int.Parse(USERDEPARTMENTID) == 5)
+                         select r;
+            } 
             else
-            { 
-            table = from r in (new dbMyCompanyContext()).TRepairs
-                    orderby r.CAppleDate descending
-                    select r;
-            }
+            {
+                if (!string.IsNullOrEmpty(Request.ContentType))
+                            {
+                                string Srepairnumber = Request.Form["repairnumber"];
+                                string SrepairdateStart = Request.Form["repairdateStart"];
+                                string SrepairdateEnd = Request.Form["repairdateEnd"];
+                                string Srepaircontent = Request.Form["repaircontent"];
+
+                                //單號不為空
+                                if (!string.IsNullOrEmpty(Srepairnumber))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CRepairNumber == (int.Parse(Srepairnumber)) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }//開始日期 結束日期 內容不為空
+                                else if (!string.IsNullOrEmpty(SrepairdateStart) && !string.IsNullOrEmpty(SrepairdateEnd) && !string.IsNullOrEmpty(Srepaircontent))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CContentofRepair.Contains(Srepaircontent) && r.CAppleDate <= DateTime.Parse(SrepairdateEnd) && r.CAppleDate >= DateTime.Parse(SrepairdateStart) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }//開始日期 結束日期不為空
+                                else if (!string.IsNullOrEmpty(SrepairdateStart) && !string.IsNullOrEmpty(SrepairdateEnd))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CAppleDate <= DateTime.Parse(SrepairdateEnd) && r.CAppleDate >= DateTime.Parse(SrepairdateStart) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }//開始日期  內容不為空
+                                else if (!string.IsNullOrEmpty(SrepairdateStart) && !string.IsNullOrEmpty(Srepaircontent))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CAppleDate >= DateTime.Parse(SrepairdateStart) && r.CContentofRepair.Contains(Srepaircontent) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }
+                                // 結束日期 內容不為空
+                                else if ( !string.IsNullOrEmpty(SrepairdateEnd) && !string.IsNullOrEmpty(Srepaircontent))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CAppleDate <= DateTime.Parse(SrepairdateEnd) && r.CContentofRepair.Contains(Srepaircontent) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }//開始日期不為空
+                                else if (!string.IsNullOrEmpty(SrepairdateStart))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CAppleDate >= DateTime.Parse(SrepairdateStart) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }//結束日期不為空
+                                else if (!string.IsNullOrEmpty(SrepairdateEnd))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CAppleDate <= DateTime.Parse(SrepairdateEnd) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }//內容不為空
+                                else
+                                if (!string.IsNullOrEmpty(Srepaircontent))
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CContentofRepair.Contains(Srepaircontent) && r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }
+                                else
+                                {
+                                    table = from r in (new dbMyCompanyContext()).TRepairs
+                                            where r.CEmployeeId == userid
+                                            orderby r.CAppleDate descending
+                                            select r;
+                                }
+                            }
+                            else
+                            { 
+                            table = from r in (new dbMyCompanyContext()).TRepairs
+                                    where r.CEmployeeId== userid
+                                    orderby r.CAppleDate descending
+                                    select r;
+                            }
+            }    
+            
 
                  
             
             List<CReairViewModel> list = new List<CReairViewModel>();
             foreach (TRepair i in table)
-                list.Add(new CReairViewModel(i));
+                list.Add(new CReairViewModel(i, null));
             return View(list);
             //return View(table);
         }
@@ -147,7 +161,7 @@ namespace MyHR_Web.Views.Home
 
                 if (repair != null)
                 {
-                    return View(new CReairViewModel(repair));
+                    return View(new CReairViewModel(repair, null));
                 }
 
             }
--- MyHR_Web/ViewModel/CReairViewModel.cs
+++ MyHR_Web/ViewModel/CReairViewModel.cs
@@ -12,15 +12,21 @@ namespace MyHR_Web.ViewModel
     public class CReairViewModel
 
     {
+        dbMyCompanyContext db = new dbMyCompanyContext();
         private TRepair iv_repair = null;
+        private TUser iv_User = null;
+
         public TRepair repair { get { return iv_repair; } }
-        public CReairViewModel(TRepair p)
+        public TUser user { get { return iv_User; } }
+        public CReairViewModel(TRepair p,TUser u)
         {
             iv_repair = p;
+            iv_User = u;
         }
         public CReairViewModel()
         {
             iv_repair = new TRepair();
+            iv_User = new TUser();
         }
 
 
@@ -39,6 +45,20 @@ namespace MyHR_Web.ViewModel
             get { return iv_repair.CEmployeeId; }
             set { iv_repair.CEmployeeId = (int)value; }
         }
+
+        [DisplayName("員工名稱")] 
+        public string CEmployeeName
+        {
+            get
+            {
+                iv_User.CEmployeeName = db.TUsers.Where(n => n.CEmployeeId == CEmployeeId).Select(n => n.CEmployeeName).FirstOrDefault();
+                return iv_User.CEmployeeName;
+            }
+            set { iv_User.CEmployeeName = value; }
+        }
+
+
+
         [Required(ErrorMessage = "必填欄位")]
         [DisplayName("報修申請日期")]
         public DateTime? CAppleDate
@@ -90,7 +110,7 @@ namespace MyHR_Web.ViewModel
         //}
 
 
-
+        public string employeeName { get { return iv_User.CEmployeeName; } set { iv_User.CEmployeeName = value; } }
 
 
 
