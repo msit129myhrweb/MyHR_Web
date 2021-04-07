a40cf42f17eb79cde42f306ecb450ca9d0d36c80
JannaSung <njrup66745@gmail.com>
2021-04-08 02:13:57 +0800

    輪播牆左右鍵待修正、換圖片

---
+++ MyHR_Web/wwwroot/MyJs/Index_Slider.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $('.autoplay').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        centerMode: true,
+        centerPadding: '0px',
+    });
+
+})
