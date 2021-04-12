ca148b36c1b7d71cde8941864bef28908f067da9
Ting Yu 張 <tina89108@yahoo.com.tw>
2021-04-12 21:05:27 +0800

    Ajax - Chart

---
+++ MyHR_Web/ViewModel/SalaryChar_Json.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHR_Web.ViewModel
+{
+    public class SalaryChar_Json
+    {
+
+
+        public List<string> name { get; set; }
+        public List<int> money { get; set; }
+
+        //public int salaryChar_Amont { get; set; }
+
+        //public string salaryChar_Name { get; set; }
+
+
+    }
+}
