cb2c1a485be0d471932f20fe6adf943e6adcb2be
AchieShen <rovingwindownload@gmail.com>
2021-04-20 10:35:28 +0800

    fix register.cshtml

---
+++ MyHR_Web/wwwroot/MyJs/DemoKey_InterviewProcess.js
@@ -0,0 +1,41 @@
+﻿$(document).keyup(function (e) {
+    if (e.shiftKey && e.keyCode == 49 ) { //Shift + 1
+        $("#idProcess").val("哈哈");
+    }
+    else if (e.shiftKey && e.keyCode == 50) { //Shift + 2
+        $("#idProcess").val("哈哈2");
+        
+    }
+    else if (e.shiftKey && e.keyCode == 51) { //Shift + 3
+        $("#idProcess").val("哈哈3");
+    }
+    else  if (e.shiftKey && e.keyCode == 52) { //Shift + 4
+        $("#idProcess").val("哈哈4");
+    }
+    else if (e.shiftKey && e.keyCode == 53) { //Shift + 5
+        $("#idProcess").val("哈哈5");
+    }
+    else if (e.shiftKey && e.keyCode == 54) { //Shift + 6
+        $("#idProcess").val("哈哈6");
+    }
+    else if (e.shiftKey && e.keyCode == 55) { //Shift + 7
+        $("#idProcess").val("哈哈7");
+    }
+    else if (e.shiftKey && e.keyCode == 56) { //Shift + 8
+        $("#idProcess").val("哈哈8");
+    }
+    else if (e.shiftKey && e.keyCode == 57) { //Shift + 9
+        $("#idProcess").val("哈哈9");
+    }
+    else if (e.shiftKey && e.keyCode == 48) { //Shift + 0
+        $("#idProcess").val("哈哈10");
+    }
+    //else if (e.shiftKey && e.keyCode == 81) { //Shift + Q
+    //    $("#txtAccount").val(15);
+    //    $("#txtPassword").val(5768325);
+    //}
+    //else if (e.shiftKey && e.keyCode == 87) { //Shift + W
+    //    $("#txtAccount").val(17);
+    //    $("#txtPassword").val(54742);
+    //}
+});
